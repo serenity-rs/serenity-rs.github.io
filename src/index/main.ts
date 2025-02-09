92ae4487ea5168a82f7f59ed867706e9d121f071
Alex M. M. <acdenissk69@gmail.com>
2025-02-09 20:14:42 +0100

    Replace redirect with a proper site

---
+++ src/index/main.ts
@@ -0,0 +1,73 @@
+function getRootElement(): HTMLElement {
+    const elem = document.querySelector<HTMLElement>(":root");
+    if (elem === null) {
+        return document.querySelector<HTMLElement>("html")!;
+    }
+
+    return elem;
+}
+
+enum Theme {
+    Light = "light",
+    Dark = "dark",
+}
+
+function getTheme(): Theme {
+    const elem = getRootElement();
+
+    const defaultTheme = Theme.Light;
+
+    switch (elem.dataset.theme) {
+        case "light":
+            return Theme.Light;
+        case "dark":
+            return Theme.Dark;
+        default:
+            const theme = localStorage.getItem("theme");
+            if (theme === null) {
+                return defaultTheme;
+            }
+
+            switch (theme) {
+                case "light":
+                    return Theme.Light;
+                case "dark":
+                    return Theme.Dark;
+                default:
+                    return defaultTheme;
+            }
+    }
+}
+
+function setTheme(theme: Theme): void {
+    const elem = getRootElement();
+
+    elem.dataset.theme = theme;
+
+    localStorage.setItem("theme", theme);
+}
+
+function toggleTheme(): void {
+    const theme = getTheme();
+
+    switch (theme) {
+        case Theme.Light:
+            setTheme(Theme.Dark);
+            break;
+        case Theme.Dark:
+            setTheme(Theme.Light);
+            break;
+    }
+}
+
+function initialise(): void {
+    const toggler = document.getElementById("toggle-button")!;
+
+    toggler.addEventListener("click", ev => {
+        ev.preventDefault();
+
+        toggleTheme();
+    });
+}
+
+window.addEventListener("load", initialise, false);
---
+++ vite.config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from "vite";
+
+import presetenv, { type DirectionFlow } from "postcss-preset-env";
+
+import { resolve } from "node:path";
+
+const assetsDir = "assets/";
+
+export default defineConfig({
+    root: resolve(__dirname, "src"),
+    publicDir: resolve(__dirname, "public"),
+    base: "/",
+    server: {
+        host: "localhost",
+        port: 3000,
+        open: false,
+        watch: {
+            usePolling: true,
+            disableGlobbing: false,
+        },
+    },
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [
+                presetenv({
+                    logical: {
+                        inlineDirection: "left-to-right" as DirectionFlow,
+                        blockDirection: "top-to-bottom" as DirectionFlow,
+                    },
+                }),
+            ],
+        },
+    },
+    build: {
+        outDir: resolve(__dirname, "docs"),
+        assetsDir,
+        emptyOutDir: true,
+        target: "es2015",
+        rollupOptions: {
+            input: [resolve(__dirname, "src", "index.html")],
+            output: {
+                entryFileNames: `${assetsDir}[name].js`,
+                chunkFileNames: `${assetsDir}[name].[ext]`,
+                assetFileNames: `${assetsDir}[name].[ext]`,
+            },
+        },
+    },
+});
